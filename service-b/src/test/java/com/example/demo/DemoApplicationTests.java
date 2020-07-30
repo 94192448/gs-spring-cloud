0a6e9ef4297520a62deef5a7d8e115b2cf815f8c
Administrator <admin@example.com>
2020-07-30 11:27:37 +0800

    feat(yml): add yaml support

--- service-b/src/test/java/com/example/demo/DemoApplicationTests.java
+++ service-b/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,8 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StringUtils;
+import org.yaml.snakeyaml.Yaml;
 
-import java.util.Date;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -59,4 +64,62 @@ public class DemoApplicationTests {
 		apolloOpenApiClient.publishNamespace("test-a","DEV","default","application",dto);
 
 	}
+
+
+	//加载yml中所有属性到apollo中
+	@Test
+	public void   loadSnakeyaml() throws FileNotFoundException {
+
+		Map<String,String> result = new HashMap<>();
+
+		InputStream inputStream = new FileInputStream("/Users/zqy/codes/yusys/tmp/gs-spring-cloud/service-b/src/test/resources/application.yml");
+
+		Yaml yaml = new Yaml();
+
+		Map<String, Object> param = (Map<String, Object>) yaml.load(inputStream);
+
+		for(Map.Entry<String,Object> entry:param.entrySet()){
+			String key = entry.getKey();
+			Object val = entry.getValue();
+			if(val instanceof Map){
+				forEachYaml(key,(Map<String, Object>) val,result);
+			}else{
+				result.put(key, val.toString());
+			}
+		}
+
+		//TODO 自定义循环调用openapi
+		result.keySet().forEach(k->{
+			log.info(k+"-----"+result.get(k));
+
+			OpenItemDTO dto = new OpenItemDTO();
+			dto.setKey(k);
+			dto.setValue(result.get(k));
+			dto.setDataChangeLastModifiedBy("apollo");
+			dto.setDataChangeCreatedBy("apollo");
+
+			apolloOpenApiClient.createOrUpdateItem("test-a","DEV","default","application",dto);
+		});
+
+	}
+
+
+	public  void forEachYaml(String key_str,Map<String, Object> obj,Map<String,String> result ){
+		for(Map.Entry<String,Object> entry:obj.entrySet()){
+			String key = entry.getKey();
+			Object val = entry.getValue();
+			String str_new = "";
+			if(!StringUtils.isEmpty(key_str)){
+				str_new = key_str+ "."+key;
+			}else{
+				str_new = key;
+			}
+			if(val instanceof Map){
+				forEachYaml(str_new,(Map<String, Object>) val,result);
+			}else{
+				result.put(str_new,val.toString());
+			}
+		}
+	}
+
 }
